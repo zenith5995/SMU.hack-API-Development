667470593345004f7a79ba79767be856cf7a6533
Keegan Ravindran <keegan.r.personal@gmail.com>
2024-06-28 21:45:51 +0800

    committed during the workshop

--- code/security/database.py
+++ code/security/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Database Setup
-SQLALCHEMY_DATABASE_URL = 'sqlite+pysqlite:///./db.sqlite3:'
+SQLALCHEMY_DATABASE_URL = 'sqlite+pysqlite:///./db.sqlite3'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
 
 # Initialize Session
